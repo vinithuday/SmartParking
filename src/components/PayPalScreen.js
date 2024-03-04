b42bf692c0d61e6dc11d2c6e0a17c6924895152b
vinithuday <vinithrajbmu@gmail.com>
2024-03-04 21:13:58 +0100

    paypal screen.js

---
+++ src/components/PayPalScreen.js
@@ -0,0 +1,37 @@
+
+
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import { View, StyleSheet, Image } from 'react-native';
+
+
+
+
+const PayPalScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <Footer/>
+      <Image
+          source={require("../../assets/paypal.webp")}
+          style={styles.logo}
+        />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    height: 250,
+    width: 400,
+  }
+});
+
+export default PayPalScreen;
