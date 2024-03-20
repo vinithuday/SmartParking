612fb828119159c39c2d010246d6133b75bf7b54
vinithuday <vinithrajbmu@gmail.com>
2024-03-20 22:26:47 +0100

    updated parking history

--- src/components/ParkingHistory.js
+++ src/components/ParkingHistory.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Header from "./Header";
 import Footer from "./Footer";
 import axios from "axios";
@@ -16,25 +16,34 @@ const ParkingHistoryScreen = () => {
         const response = await axios.post(API.getParkingHistory, {
           email: email,
         });
-        console.log(response.data.data);
         setParkingHistory(response.data.data);
       } catch (error) {
         console.error("Error fetching parking history:", error.message);
       }
     };
-
+  
     fetchParkingHistory();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Header />
-      <Text style={styles.title}>Parking History</Text>
+    <Header />
+    <Footer/>
+    <Text style={styles.title}>Parking History</Text>
+    <ScrollView style={styles.scrollView}>
+    <View style={styles.cardContainer}> 
       {parkingHistory.map((historyItem, index) => (
-        <View key={index} style={styles.historyItem}>
-          <Footer />
+        <View key={index} style={styles.card}>
+          <Text>Email: {historyItem.email}</Text>
+          <Text>Location: {historyItem.location}</Text>
+          <Text>Arrival Time: {historyItem.arrivalDateTime}</Text>
+          <Text>Departure Time: {historyItem.departureTime}</Text>
+          <Text>Selected Slot: {historyItem.selectedSlot}</Text>
+          <Text>Total Price: {historyItem.totalPrice}</Text>
         </View>
       ))}
+      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -49,14 +58,37 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 10,
+    color: "#4595E0",
+    top: 95,
   },
-  historyItem: {
-    marginVertical: 5,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "#ddd",
-    borderRadius: 5,
+  scrollView: {
+    width: "100%",
+    marginTop: 10,
+    marginBottom: 80, 
+    marginTop: 120,
+
+  },
+  cardContainer: {
+    alignItems: "center", 
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 45,
+    marginBottom: 10,
+    width: "90%", 
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  cardText: {
+    fontSize: 16,
+    marginBottom: 5,
   },
 });
 
